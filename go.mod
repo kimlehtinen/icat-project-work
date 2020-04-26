129b73b2debd16e1cbcb86eb0c6e61d2bd798d6f
kim3z <kim.lehtinen@hotmail.com>
2020-04-26 17:23:49 +0300

    Basic go server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kim3z/icat-project-work
+
+go 1.12
+
+require github.com/gorilla/mux v1.7.4
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Server running")
+	})
+
+	fmt.Println("Server listening!")
+	http.ListenAndServe(":8080", r)
+}
